820b5311249b2047505eaceb0b73d1d442d0e906
ignac8 <ignac8@op.pl>
2021-12-11 21:00:46 +0100

    Small fixes

--- new-team-chooser/src/main/java/it/zerko/teamchooser/Main.java
+++ new-team-chooser/src/main/java/it/zerko/teamchooser/Main.java
@@ -15,29 +15,31 @@ public class Main {
   public static void main(String[] args) throws IOException {
     int teamSize = 5;
 
-    List<Player> manualPlayers = Stream.of(new Player[]{
-//     new Player("kolega Johnego", 1.0),
-    }).collect(Collectors.toList());
-
     List<Player> automaticPlayers = Files.readAllLines(Paths.get("results.tsv"))
       .stream()
+      .filter(line -> !line.isBlank())
       .map(Player::new)
       .collect(Collectors.toList());
 
     automaticPlayers.forEach(player -> System.out.println(Double.toString(player.getRating()).replace(".", ",")));
+
+    List<Player> manualPlayers = Stream.of(new Player[]{
+//      new Player("Walo", 1)
+    }).collect(Collectors.toList());
 
     List<Player> players = Stream.concat(manualPlayers.stream(), automaticPlayers.stream())
       .limit(teamSize * 2)
       .collect(Collectors.toList());
 
     Combination bestCombination = CombinatoricsFactory.createSimpleCombinationGenerator(
-      CombinatoricsFactory.createVector(players), teamSize)
+        CombinatoricsFactory.createVector(players), teamSize)
       .generateAllObjects()
       .stream()
       .map(combination -> new Combination(players, combination))
       .filter(combination -> keepTogether(combination, "ignac8", "Koala", "Baleron"))
       .filter(combination -> keepTogether(combination, "Mr. Sandman", "yayebie"))
       .filter(combination -> !keepTogether(combination, "dzast.da.breslau", "Mr. Sandman"))
+      .filter(Main::firstTeamWithIgnac)
       .filter(combination -> combination.getFirstTeam().getPlayers().size() == teamSize)
       .sorted()
       .findFirst()
@@ -46,6 +48,10 @@ public class Main {
     System.out.println(bestCombination);
 
     boolean debug = true;
+  }
+
+  private static boolean firstTeamWithIgnac(Combination combination) {
+    return combination.getFirstTeam().getPlayers().stream().anyMatch(player -> player.getNickname().equals("ignac8"));
   }
 
   private static boolean keepTogether(Combination combination, String... nicknames) {
